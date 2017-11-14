2d2a3f53250d76f96934521f230dd87494ffe201
Cyberjaya 4 iTrain <cyberjaya4itrain@Cyberjayas-iMac-6.local>
2017-11-14 09:37:24 +0800

    Initial Commit

---
+++ MultiTab/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MultiTab
+//
+//  Created by Cyberjaya 4 iTrain on 14/11/2017.
+//  Copyright © 2017 ada_yc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
